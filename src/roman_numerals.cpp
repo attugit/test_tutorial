1a0dac21f415ff07d58bf323bf15f703a69fb317
Karol Wozniak <karol.wozniak@email.com>
2016-05-05 20:49:12 +0200

    test for IX and X

--- src/roman_numerals.cpp
+++ src/roman_numerals.cpp
@@ -5,5 +5,7 @@ std::string xcode::to_roman(std::size_t n) {
   auto ret = std::string(n, 'I');
   boost::replace_all(ret, "IIIII", "V");
   boost::replace_all(ret, "IIII", "IV");
+  boost::replace_all(ret, "VV", "X");
+  boost::replace_all(ret, "VIV", "IX");
   return ret;
 }
--- test/03_roman_numerals/roman_numerals.cpp
+++ test/03_roman_numerals/roman_numerals.cpp
@@ -18,4 +18,14 @@ TEST_F(roman_test, useIVToRepresentFourAndVToRepresentFive) {
   EXPECT_EQ(to_roman(7), "VII");
   EXPECT_EQ(to_roman(8), "VIII");
 }
+
+TEST_F(roman_test, useIXToRepresentNineAndXToRepresentTen) {
+  EXPECT_EQ(to_roman(9), "IX");
+  EXPECT_EQ(to_roman(10), "X");
+  EXPECT_EQ(to_roman(11), "XI");
+  EXPECT_EQ(to_roman(12), "XII");
+  EXPECT_EQ(to_roman(13), "XIII");
+  EXPECT_EQ(to_roman(14), "XIV");
+  EXPECT_EQ(to_roman(15), "XV");
+}
 }
